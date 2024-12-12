b641db148f8c87fc3d7cb8c2a9f8777089a48ef3
Heather Spacek <jdmls27@gmail.com>
2024-12-12 16:17:46 -0700

    figured out identifiers, but . hmmmmm janky
    i don't like "get parent of get parent" (line 14). should re-visit this later but its decent right now

--- dev/strategyplayer_tool_templates.py
+++ dev/strategyplayer_tool_templates.py
@@ -4,23 +4,28 @@ import strategyplayer_tool_callbacks as cbx
 
 def trigger_template(parent, trigger):
     with dpg.collapsing_header(label="Trigger N", user_data=trigger,
-                               parent=parent, indent=20):
+                               parent=parent, indent=20) as H1:
+        header_id = str(H1)
+
         def trigger_type_select_callback(sender, value, user_data):
             # Based on box selection, change the state of the template, and
             # also
+            sender_header_id = str(
+                    dpg.get_item_parent(dpg.get_item_parent(sender))
+                )
 
             if value == "TimeTrigger":
-                dpg.configure_item("_timegroup", show=True)
-                dpg.configure_item("_distgroup", show=False)
-                dpg.configure_item("_actiongroup", show=False)
+                dpg.configure_item("timegroup_"+sender_header_id, show=True)
+                dpg.configure_item("distgroup_"+sender_header_id, show=False)
+                dpg.configure_item("actiongroup_"+sender_header_id, show=False)
             elif value == "DistanceTrigger":
-                dpg.configure_item("_timegroup", show=False)
-                dpg.configure_item("_distgroup", show=True)
-                dpg.configure_item("_actiongroup", show=False)
+                dpg.configure_item("timegroup_"+sender_header_id, show=False)
+                dpg.configure_item("distgroup_"+sender_header_id, show=True)
+                dpg.configure_item("actiongroup_"+sender_header_id, show=False)
             elif value == "ActionTrigger":
-                dpg.configure_item("_timegroup", show=False)
-                dpg.configure_item("_distgroup", show=False)
-                dpg.configure_item("_actiongroup", show=True)
+                dpg.configure_item("timegroup_"+sender_header_id, show=False)
+                dpg.configure_item("distgroup_"+sender_header_id, show=False)
+                dpg.configure_item("actiongroup_"+sender_header_id, show=True)
             else:
                 pass
 
@@ -28,14 +33,15 @@ def trigger_template(parent, trigger):
             dpg.add_combo(
                     ["TimeTrigger", "DistanceTrigger", "ActionTrigger"],
                     label="Trigger Type",
-                    callback=trigger_type_select_callback
+                    callback=trigger_type_select_callback,
+                    user_data=()
                     )
             dpg.add_button(label=" × ", callback=cbx.callback_delete_trigger,
                            user_data=trigger)
-        with dpg.group(show=False, tag="_timegroup"):
+        with dpg.group(show=False, tag="timegroup_"+header_id):
             dpg.add_input_int(label="time value")
-        with dpg.group(show=False, tag="_distgroup"):
+        with dpg.group(show=False, tag="distgroup_"+header_id):
             dpg.add_input_int(label="distance value")
-        with dpg.group(show=False, tag="_actiongroup"):
+        with dpg.group(show=False, tag="actiongroup_"+header_id):
             dpg.add_input_int(label="frame value")
 
