db007b983ce2d28a81360101aed094f0040d7277
Heather Spacek <heather.spacek@helpwear.ca>
2026-03-12 10:21:56 -0600

    sandbox to avoid circular imports perhaps

---
+++ sandbox.py
@@ -0,0 +1,10 @@
+from src.messlib.interfaces.host import Host
+from src.messlib.interfaces.situation import goto
+
+host = Host()
+host.setup()
+
+for _ in range(500):
+    _ = host.console.step()
+
+goto(0,0,False,False,host)
