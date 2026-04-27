d34e26126197521ac7dc1bbee35d12e59eab6e79
Heather Spacek <heather.spacek@helpwear.ca>
2026-04-27 09:46:09 -0600

    payofftool results viewer: data-domain cursor reaction

--- src/payofftool/tool.py
+++ src/payofftool/tool.py
@@ -6,12 +6,15 @@ from ..messlib.data_structures.situation import Situation
 from .solver import PayoffSolver
 
 from mess.animations.vis import lerp_2d
-from mess.animations._rs import dump_one_character
+from mess.animations.data import retrieve_move_data, HurtBoxProcessed
+from .structures import PayoffReplayFrame
+
+import numpy as np
 
 
 def ptool_setup_window():
 
-    with dpg.window(tag="win"):
+    with dpg.window(tag="win_setup", pos=(0, 0)):
         dpg.add_spacer(tag="host_dummy")
         dpg.add_spacer(tag="solver_dummy")
 
@@ -24,8 +27,8 @@ def ptool_setup_window():
         dpg.add_slider_float(label="P2 Percent", tag="p2p", max_value=300)
 
         dpg.add_separator()
-        dpg.add_text("<Later, there will be fields here to set position (etc)>..."
-                     "")
+        with dpg.drawlist(tag="setup_dlist", width=250, height=200):
+            dpg.draw_rectangle(pmin=(5, 5), pmax=(245, 195))
 
         dpg.add_button(
             label="Close and run sim",
@@ -58,29 +61,130 @@ def parse_from_window_settings() -> Situation:
 
 
 def ptool_results_window():
-
     with dpg.window(tag="win_res", show=False):
         dpg.add_text("(Results go here.)", tag="resultsprint")
         with dpg.group(horizontal=True):
-            with dpg.drawlist(width=300, height=200, tag="canvas", parent="ttip"):
+            with dpg.drawlist(width=300, height=200, tag="canvas"):
                 dpg.draw_rectangle(pmin=[10, 10], pmax=[290, 190])
             with dpg.plot(no_mouse_pos=True, height=200, width=300, tag="PLT"):
-                dpg.add_plot_axis(dpg.mvXAxis, label="fox timing", lock_min=True, lock_max=True)
-                with dpg.plot_axis(dpg.mvYAxis, label="falco timing", lock_min=True, lock_max=True):
-                    dpg.add_heat_series([1.0, 0.0, 0.4, 0.5], 2, 2)
+                dpg.add_plot_axis(dpg.mvXAxis, label="fox timing", lock_min=True, lock_max=True, tag="plt_xaxis")
+                with dpg.plot_axis(dpg.mvYAxis, label="falco timing", lock_min=True, lock_max=True, tag="plt_yaxis"):
+                    dpg.add_heat_series([0.0], 1, 1, tag="plt_series", col_major=True)
                     with dpg.tooltip(dpg.last_item(), tag="ttip"):
-                        dpg.add_text("Tooltip text.", tag="tooltext")
+                        dpg.add_text("", tag="tooltext")
 
 
 def go_callback():
     # compose Situation Struct from the window contents:
     slvr: PayoffSolver = dpg.get_item_user_data("solver_dummy")
     slvr.situation = parse_from_window_settings()
-
     slvr._debug_solve()
 
+    # dpg.set_value("resultsprint", slvr.results)
     dpg.show_item("win_res")
-    dpg.set_value("resultsprint", slvr.results)
+    display_results(slvr.results)
+
+
+def display_results(solver_results):
+    """Configure the results window, the plot series, the replay view,
+    etc etc. The structure of solver_results is {k: v} where k is the
+    tuple of (x,y) to plot, and v is (outcome, [frames_list])"""
+    OUTCOME_MAPPING = {
+        "Falco win": 1.0,
+        "Fox win": 0.0,
+        "Whiff": 0.5
+    }
+    outcomes_numeric = [OUTCOME_MAPPING[v[0]] for v in solver_results.values()]
+    outcomes_x = set([k[0] for k in solver_results.keys()])
+    outcomes_y = set([k[1] for k in solver_results.keys()])
+    dpg.configure_item(
+        "plt_series",
+        cols=len(outcomes_x),
+        rows=len(outcomes_y),
+        x=outcomes_numeric,
+    )
+    dpg.set_item_user_data("plt_series", outcomes_numeric)
+    autoticks_x = np.arange(0, 1, 0.5/(len(outcomes_x)))[1::2]
+    tickmap_x = tuple((str(k), v) for k, v in zip(sorted(outcomes_x), autoticks_x))
+    dpg.set_axis_ticks("plt_xaxis", label_pairs=tickmap_x)
+    dpg.set_item_user_data("plt_xaxis", tickmap_x)
+    autoticks_y = np.arange(0, 1, 0.5/(len(outcomes_y)))[1::2]
+    tickmap_y = tuple((str(k), v) for k, v in zip(sorted(outcomes_y), autoticks_y))
+    dpg.set_axis_ticks("plt_yaxis", label_pairs=tickmap_y)
+    dpg.set_item_user_data("plt_yaxis", tickmap_y)
+
+
+def mouseover_plot_react(mouse_coords):
+    plt_val = dpg.get_item_user_data("plt_series")
+    plt_x = np.array([xi[1] for xi in dpg.get_item_user_data("plt_xaxis")])
+    bound_x = plt_x - plt_x[0]
+    plt_y = np.array([yi[1] for yi in dpg.get_item_user_data("plt_yaxis")])
+    bound_y = plt_y - plt_y[0]
+    shaped = np.reshape(plt_val, (len(plt_x), len(plt_y)))
+    sel_x = np.argmax(bound_x > mouse_coords[0]) - 1
+    sel_y = np.argmax(bound_y > mouse_coords[1]) - 1
+    shaped[sel_x, -sel_y-1] = 0.27
+    dpg.configure_item("plt_series", x=shaped.flatten().tolist())
+
+    res = dpg.get_item_user_data("solver_dummy").results
+    # Sentinel value... TODO fixup
+    hover_key = list(res.keys())[np.argmax(shaped == 0.27)]
+    dpg.set_value("tooltext", res[hover_key][0])
+    dpg.set_value("resultsprint", hover_key)
+    # just in case its casting to string implicitly:
+    dpg.set_item_user_data("resultsprint", hover_key)
+
+
+def dpg_draw_capsule(y1, z1, y2, z2, size):
+    for t in [a/9 for a in range(10)]:
+        x, y = lerp_2d((y1, z1), (y2, z2), t)
+        dpg.draw_circle([x, y], size, parent="canvas")
+
+
+def draw_replay_frame():
+    res = dpg.get_item_user_data("solver_dummy").results    
+    if res is None:
+        return  # no results yet.
+
+    results_index_tuple = dpg.get_item_user_data("resultsprint")
+    if results_index_tuple is None:
+        results_index_tuple = list(res.keys())[0]
+
+    replay = res[results_index_tuple][1]
+    repl_frame_to_draw: PayoffReplayFrame = replay[dpg.get_frame_count() % len(replay)]
+
+    p1x = repl_frame_to_draw.p1_pos.x
+    p2x = repl_frame_to_draw.p2_pos.x
+
+    hurts1, hits1 = retrieve_move_data(
+        "/home/heather/Documents/Disk Images/Super Smash Bros. Melee (v1.02).iso",
+        1,
+        repl_frame_to_draw.p1_game_action.value
+    )
+    hurts2, hits2 = retrieve_move_data(
+        "/home/heather/Documents/Disk Images/Super Smash Bros. Melee (v1.02).iso",
+        22,
+        repl_frame_to_draw.p2_game_action.value
+    )
+
+    hurts1_thisframe: list[HurtBoxProcessed] = hurts1[repl_frame_to_draw.p1_game_action_frame]
+    hurts2_thisframe: list[HurtBoxProcessed] = hurts2[repl_frame_to_draw.p2_game_action_frame]
+
+    dpg.delete_item("canvas", children_only=True)
+    dpg.draw_rectangle(pmin=[10, 10], pmax=[290, 190], parent="canvas")
+
+    DRAW_SCALE = 4
+    for hx in hurts1_thisframe:
+        x1, y1, z1 = hx.pos_a
+        x2, y2, z2 = hx.pos_b
+        scale = hx.size
+        dpg_draw_capsule(
+            (10+z1+p1x) * DRAW_SCALE,
+            135-y1 * DRAW_SCALE,
+            (10+z2+p1x) * DRAW_SCALE,
+            135-y2 * DRAW_SCALE,
+            scale * DRAW_SCALE
+        )
 
 
 if __name__ == "__main__":
@@ -94,7 +198,6 @@ if __name__ == "__main__":
     dpg.create_viewport(title="Tool1", width=300, height=200)
     dpg.setup_dearpygui()
     dpg.show_viewport()
-    # dpg.set_primary_window("win", True)
 
     # Data objects...
     host = Host()
@@ -106,20 +209,9 @@ if __name__ == "__main__":
     while dpg.is_dearpygui_running():
         if dpg.is_item_shown("canvas"):
             mouse_coords = dpg.get_plot_mouse_pos()
-            res = dpg.get_item_user_data("solver_dummy").results
-
-            # value_under_mouse = res[tuple(mouse_coords)]
-            value_under_mouse = mouse_coords
-            dpg.set_value("tooltext", str(value_under_mouse))
-            dpg.delete_item("canvas", children_only=True)
-            dpg.draw_rectangle(pmin=[10, 10], pmax=[290, 190], parent="canvas")
-            dpg.draw_circle(
-                center=(100 * mouse_coords[0], 200-(100 * mouse_coords[1])),
-                radius=10,
-                parent="canvas"
-            )
-            # if mouse_coords[0] > 0.1:
-            #     breakpoint()
+            if mouse_coords[0] > 0.0:
+                mouseover_plot_react(mouse_coords)
+            draw_replay_frame()
 
         dpg.render_dearpygui_frame()
     dpg.destroy_context()
