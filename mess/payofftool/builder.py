5bfa6e5e7cdab95e417d05f7548e961ae85b4762
Heather Spacek <heatherspacek1997@gmail.com>
2026-08-13 23:05:49 -0600

    fun with callbacks

--- mess/payofftool/builder.py
+++ mess/payofftool/builder.py
@@ -1,40 +1,46 @@
 """
 interface for building detailed sequences. 
-we'll see what we can do with DPG, and if it's too restrictive, investigate smth else.
+
+related scratchpad...
+
 """
 import dearpygui.dearpygui as dpg
 
 def clamped_lighten(col_in: tuple[int], by: int):
     return tuple(c + by if c + by <= 255 else 255 for c in col_in)
 
+TICK_PIXELS_W = 15
+TICK_PIXELS_H = 25
+
 class Segment:
-    def __init__(self, color_idle= (0,150,200), tooltip_contents = "Placeholder.", width=25):
+    def __init__(self, color_idle= (0,150,200), tooltip_contents = "Placeholder.", n_ticks=1):
         self.color_idle = color_idle
         self.color_hover = clamped_lighten(color_idle, 15)
         self.color_active = clamped_lighten(color_idle, 25)
         self.tooltip_contents = tooltip_contents
-        self.width = width
+        self.n_ticks = n_ticks
 
         with dpg.theme() as new_theme:
             with dpg.theme_component(dpg.mvAll):
                 dpg.add_theme_color(dpg.mvThemeCol_Button, self.color_idle)
                 dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, self.color_hover)
                 dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, self.color_active)
-                dpg.add_theme_color(dpg.mvThemeCol_BorderShadow, (255,255,255))
 
                 dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5)
                 dpg.add_theme_style(dpg.mvStyleVar_FrameBorderSize, 0)
         self.theme = new_theme
+        self.button_ref = None
+        self.button_cb = None
 
     def add_to_ui(self):
-        self._button_ref = dpg.add_button(width=self.width, height=25)
-        dpg.bind_item_theme(dpg.last_item(), self.theme)
-        with dpg.tooltip(self._button_ref):
+        ref = dpg.add_button(width=TICK_PIXELS_W*self.n_ticks, height=TICK_PIXELS_H, callback=self.button_cb)
+        dpg.bind_item_theme(ref, self.theme)
+        self.button_ref = ref
+        with dpg.tooltip(parent=ref):
             dpg.add_text(self.tooltip_contents)
 
 
 class SegmentContainer:
-    frame_pixels = 25
 
     def __init__(self, *args):
         with dpg.theme() as new_theme:
@@ -46,24 +52,52 @@ class SegmentContainer:
         self.segments = []
         for seg in args:
             self.segments.append(seg)
+            seg.button_cb = self.parent_cb
+
+        self.infobox_ref = None
+        self.focused = None
+
+    def parent_cb(self, *args):
+        print(a for a in args)
+        dpg.configure_item(self.infobox_ref, show=True)
 
     def add_to_ui(self):
-        with dpg.group(horizontal=True) as groupref:
-            for seg in self.segments:
-                seg.add_to_ui()
-        self.groupref = groupref
-        dpg.bind_item_theme(groupref, self.theme)
+        with dpg.child_window(width=300, height=150, show=False) as infobox:
+            with dpg.group(horizontal=True):
+                dpg.add_text("Customizing Action: ")
+                dpg.add_text("", tag="info_param_action_name")
+            with dpg.group(horizontal=True):
+                dpg.add_text("Some quantity: ")
+                dpg.add_text("", tag="info_param_frames")
+            dpg.add_knob_float(label="Jump Angle")
+        with dpg.child_window(width=300, height=55, resizable_x=True, horizontal_scrollbar=True):
+            with dpg.group(horizontal=True) as groupref:
+                for seg in self.segments:
+                    seg.add_to_ui()
+            self.groupref = groupref
+            dpg.bind_item_theme(groupref, self.theme)
+        self.infobox_ref = infobox
 
-        # Timeline
-        for n in range(6):
-            dpg.add_text(str(n), pos=(15+(n*25), 50))
+    def add(self):
+        ...
+
+    def remove(self):
+        ...
+
+    def set_focus(self, seg):
+        dpg.configure_item(self.infobox_ref, show=True)
+
 
 def layout():
-    s1 = Segment((198, 98, 23), "F1", 75)
-    s2 = Segment()
-    s3 = Segment()
+
+    s1 = Segment((198, 98, 23), "F1", 5)
+    s2 = Segment(n_ticks=12)
+    s3 = Segment((100, 225, 44), "action 3", 7)
     scont = SegmentContainer(s1, s2, s3)
-    # this api is ASS will fixup later for easier adding and removing!!!!
+
+    dpg.add_button(label="Add a random action", callback=scont.add)
+    dpg.add_button(label="Remove selected action", show=False, callback=scont.remove, tag="btn_remove")
+
     scont.add_to_ui()
 
 
